3d49c6c57c84f25acfe4149aeb76b12951b04e29
jainish2027 <jn847886@dal.ca>
2019-05-23 07:36:51 -0700

    find words  form all books

--- task3.py
+++ task3.py
@@ -1,16 +1,16 @@
-def read_book(Book1.txt):
+def find_words(Book1.txt):
 	with open(Book1.txt, "r",encoding="20k.txt") as current_file:
 		text=current_file.read()
 		text=text.replace("\n","").replace("\r","")
 	return text
 
-def read_book(Book2.txt):
+def find_words(Book2.txt):
         with open(Book2.txt, "r",encoding="20k.txt") as current_file:
                 text=current_file.read()
                 text=text.replace("\n","").replace("\r","")
         return text
 
-def read_book(Book3.txt):
+def find_words(Book3.txt):
         with open(Book1.txt, "r",encoding="20k.txt") as current_file:
                 text=current_file.read()
                 text=text.replace("\n","").replace("\r","")
