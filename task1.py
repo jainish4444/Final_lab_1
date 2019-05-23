acf6dea5094916e1fedff755d1117c40e0849737
jainish2027 <jn847886@dal.ca>
2019-05-23 06:05:02 -0700

    basic code for file copy

---
+++ task1.py
@@ -0,0 +1,6 @@
+import shutil
+
+shutil.copyfile('/home/student/finallab/openbookfinal-2019-jainish4444/abc.txt', '/home/student/finallab/openbookfinal-2019-jainish4444/abc_copy2.txt')
+
+
+print("File Copy Done")
