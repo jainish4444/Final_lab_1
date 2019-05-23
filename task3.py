f1396ddf08eeb9020b57279f7812802bd33955d4
jainish2027 <jn847886@dal.ca>
2019-05-23 07:34:50 -0700

    non present words for all books

---
+++ task3.py
@@ -0,0 +1,18 @@
+def read_book(Book1.txt):
+	with open(Book1.txt, "r",encoding="20k.txt") as current_file:
+		text=current_file.read()
+		text=text.replace("\n","").replace("\r","")
+	return text
+
+def read_book(Book2.txt):
+        with open(Book2.txt, "r",encoding="20k.txt") as current_file:
+                text=current_file.read()
+                text=text.replace("\n","").replace("\r","")
+        return text
+
+def read_book(Book3.txt):
+        with open(Book1.txt, "r",encoding="20k.txt") as current_file:
+                text=current_file.read()
+                text=text.replace("\n","").replace("\r","")
+        return text
+
